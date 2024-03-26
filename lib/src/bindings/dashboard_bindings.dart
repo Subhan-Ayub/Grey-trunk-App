f7203fc8cd6c5b813190101c1cc073bd0a8a4d1c
subhan.ayub <sobimalik3344@gmail.com>
2024-03-26 14:09:15 +0500

    update folder

---
+++ lib/src/bindings/dashboard_bindings.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class DashboardBindings extends Bindings{
+    @override
+  void dependencies(){
+    // Get.lazyPut<ActivityController>(() => ActivityController());
+  }
+}
