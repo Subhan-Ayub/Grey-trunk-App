a180516f358d9a95b9a22b461c6ce26a1abf4db3
subhan.ayub <sobimalik3344@gmail.com>
2024-03-26 12:44:53 +0500

    check

--- lib/src/utils/uidata/color.dart
+++ lib/src/utils/uidata/color.dart
@@ -6,4 +6,6 @@ class UIDataColors {
   // <------------------ Just Define Common Color ------------------------------>
   static const commonColor = Color(0xFFF2194E);
   static const greyColor = Color.fromARGB(255, 244, 244, 244);
+
+  
 }
