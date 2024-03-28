d6930f85d5626e55998df5bdbac6464127ebf5a3
ijaz khan <ijazkhani615@gmail.com>
2024-03-28 00:20:04 -0700

    second commit

---
+++ lib/src/utils/uidata/appImages.dart
@@ -0,0 +1,15 @@
+class AppImage{
+  static const logo="assets/images/applogo.png";
+  static const home="assets/images/svg/home.svg";
+  static const driver="assets/images/svg/driver.svg";
+  static const passenger="assets/images/svg/passenger.svg";
+  static const revenue="assets/images/svg/revenue.svg";
+  static const rides="assets/images/svg/rides.svg";
+  static const admin="assets/images/svg/admin.svg";
+  static const acCar="assets/images/png/acCar.png";
+  static const bike="assets/images/png/bike.png";
+  static const car="assets/images/png/car.png";
+  static const nonAcCar="assets/images/png/nonAccar.png";
+  static const raksha="assets/images/png/raksha.png";
+
+}
